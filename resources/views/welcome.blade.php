2f2751bd44444260d3eb3438340dffc26cb56c8c
awallis1609@gmail.com <=>
2024-06-03 22:19:06 +0100

    fix: remove border

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -26,8 +26,7 @@
     </head>
     <body class="font-sans antialiased min-h-screen w-full bg-off-white">
 
-    <div class="h-screen p-5 md:p-10 box-border bg-my-green border-2 border-black">
-        <div class="h-full w-full flex flex-col justify-center bg-off-white border-2 border-black p-10">
+        <div class="h-screen w-full flex flex-col justify-center bg-off-white p-10">
             <h1 class="cursor-pointer font-bold text-4xl md:text-9xl text-my-red">Amanda Wallis</h1>
 
             <p class="mt-4">I create websites.</p>
@@ -37,7 +36,6 @@
                 <x-link href="https://github.com/Laptop-Salad">GitHub</x-link>
             </div>
         </div>
-    </div>
 
     <div class="flex flex-col items-center space-y-10 p-10 md:px-40 min-h-screen">
         <div class="grid md:grid-cols-3 gap-16">
