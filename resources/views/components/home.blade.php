b78f3eabb12e519dbacd7086e362a33c94a5c6cc
Laptop-Salad <awallis1609@gmail.com>
2025-01-12 21:35:36 +0000

    update bio

--- resources/views/components/home.blade.php
+++ resources/views/components/home.blade.php
@@ -5,8 +5,6 @@
         WALLIS
     </h1>
     <p class="text-xl">
-        Software Specialist currently pursuing an MSCi in Computing Science. I am a full-stack developer who specialises in designing and
-        developing large-scale applications and turning around hard to maintain software/workflows into easy-to-use software that you
-        hopefully never touch again.
+        Software Specialist currently pursuing an MSCi in Computing Science. I am a full-stack developer who works primarily Laravel + Livewire.
     </p>
 </div>
